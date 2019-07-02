bcd6c761c5e72846e6ac87c0b13ed97a25e9ce68
user-admin <matt.stephens@dvla.gov.uk>
2019-07-02 15:39:06 +0100

    swagger UI changes

--- src/main/java/com/practice/work/films/controllers/AddFilmController.java
+++ src/main/java/com/practice/work/films/controllers/AddFilmController.java
@@ -2,6 +2,7 @@ package com.practice.work.films.controllers;
 
 import com.practice.work.films.entities.Film;
 import com.practice.work.films.repositories.FilmRepository;
+import io.swagger.annotations.Api;
 import org.springframework.http.HttpStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -10,6 +11,7 @@ import javax.validation.Valid;
 
 @RestController
 @ResponseStatus(HttpStatus.CREATED)
+@Api(value = "Add Film Endpoint", description = "REST Endpoint for adding films")
 public class AddFilmController {
 
     private FilmRepository filmRepository;
