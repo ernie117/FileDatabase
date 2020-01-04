e3e223103647a2d09a5dc986af9fa5b242092a4a
ernie117 <ern3st@hotmail.co.uk>
2020-01-04 16:31:43 +0000

    application test now uses junit 5

--- src/test/java/com/practice/work/films/FilmsApplicationTests.java
+++ src/test/java/com/practice/work/films/FilmsApplicationTests.java
@@ -1,68 +1,26 @@
 package com.practice.work.films;
 
-import com.practice.work.films.controllers.TestController;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import com.practice.work.films.service.FilmsService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.hamcrest.Matchers.containsString;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
-@AutoConfigureMockMvc
 public class FilmsApplicationTests {
 
-    private MockMvc mockMvc;
-
     @Autowired
-    private TestController testController;
-
-    @Before
-    public void setup() {
-        this.mockMvc = MockMvcBuilders.standaloneSetup(testController).build();
-    }
+    private FilmsService filmsService;
 
     /**
      * Sanity-checking that context is being loaded
      */
     @Test
     public void contextLoads() {
-        assertThat(new Object[]{testController, mockMvc}).doesNotContainNull();
-    }
-
-    /**
-     * Testing that the TestController returns expected test string
-     *
-     * @throws Exception thrown by perform()
-     */
-    @Test
-    public void shouldReturnDefaultMessage() throws Exception {
-        this.mockMvc.perform(get("/v1/testing"))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType("text/plain;charset=UTF-8"))
-                .andExpect(content().string(containsString("hello world")));
-    }
-
-    /**
-     * Asserting that service returns proper JSON content and correct value
-     *
-     * @throws Exception thrown by perform()
-     */
-    @Test
-    public void returnCorrectTestEntityValue() throws Exception {
-        this.mockMvc.perform(get("/v1/testingEntity"))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
-                .andExpect(jsonPath("$.testString").value("This is a test"));
+        assertNotNull(filmsService);
     }
 }
