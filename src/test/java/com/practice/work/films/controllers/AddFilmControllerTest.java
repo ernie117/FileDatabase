9d5ee032a085552471f4a05894ee35e3382b19fb
Robert Matthew Stephens <matt.stephens@dvla.gov.uk>
2020-01-08 19:19:32 +0000

    added empty test classes for uncovered controllers, fill-in implementation next time

---
+++ src/test/java/com/practice/work/films/controllers/AddFilmControllerTest.java
@@ -0,0 +1,12 @@
+package com.practice.work.films.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AddFilmControllerTest {
+
+    @Test
+    void insertFilmDocument() {
+    }
+}
