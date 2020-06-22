a61efd67c4db2a718bd98f4206407a9862a580a8
Robert Matthew Stephens <matt.stephens@dvla.gov.uk>
2020-06-22 16:27:05 +0100

    upped to java 14, changes to allow spring-boot:build-image

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM openjdk:13-alpine
+FROM openjdk:14-alpine
 COPY target/spring-films-app.jar spring-films-app
 EXPOSE 8888
 ENTRYPOINT ["java", "--enable-preview", "-jar", "spring-films-app"]
--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-mvn clean package spring-boot:repackage
+mvn clean package spring-boot:repackage && mvn spring-boot:build-image
 
-docker-compose -f docker-compose.yml up --build
+docker-compose -f docker-compose.yml up
