155feb8674977b8537326e9343290b71e9e244d1
ernie117 <ern3st@hotmail.co.uk>
2021-01-11 16:58:17 +0000

    removing pyopenssl from functional test install

--- build-scripts/install-python.sh
+++ build-scripts/install-python.sh
@@ -9,4 +9,5 @@ sudo apt install python3.8
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.8 2
 sudo update-alternatives --set python /usr/bin/python3.8
 sudo apt install python3-pip python3-distutils-extra python3-setuptools
-pip3 install requests behave pyOpenSSL
+pip3 install requests behave
+#pip3 install pyOpenSSL
