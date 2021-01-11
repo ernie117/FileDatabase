c6bf09205d76bacd42a745bcaa19cdf05bf8f75a
ernie117 <ern3st@hotmail.co.uk>
2021-01-11 16:44:52 +0000

    removing dev http auth requirement

--- src/main/java/com/practice/work/films/configuration/SecurityConfig.java
+++ src/main/java/com/practice/work/films/configuration/SecurityConfig.java
@@ -2,14 +2,11 @@ package com.practice.work.films.configuration;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
-import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @EnableWebSecurity
 @Slf4j
@@ -26,17 +23,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .passwordEncoder(new Argon2PasswordEncoder())
-                .withUser(user)
-                .password(String.format("{argon2}%s", password))
-                .roles("ADMIN", "USER");
+        if (env.equalsIgnoreCase("prod")) {
+            auth.inMemoryAuthentication()
+                    .passwordEncoder(new Argon2PasswordEncoder())
+                    .withUser(user)
+                    .password(String.format("{argon2}%s", password))
+                    .roles("ADMIN", "USER");
+        }
     }
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
-        switch (env) {
-            case "prod" -> httpSecurity
+        if (env.equalsIgnoreCase("prod")) {
+            httpSecurity
                     .csrf().disable()
                     // .requiresChannel() When HTTPS is actually enabled on EC2
                     // .anyRequest()
@@ -47,18 +46,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .hasRole("ADMIN")
                     .and()
                     .httpBasic();
-            case "dev" -> httpSecurity
-                    .csrf().disable()
-                    .authorizeRequests()
-                    .anyRequest()
-                    .hasRole("ADMIN")
-                    .and()
-                    .httpBasic();
-            default -> httpSecurity.csrf().disable();
+        } else {
+            httpSecurity.csrf().disable();
         }
     }
 
-    // Also for when HTTPS is properly enabled, redirects HTTP to HTTPS
+//     Also for when HTTPS is properly enabled, redirects HTTP to HTTPS
 //    @Bean
 //    SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
 //        http.redirectToHttps();
