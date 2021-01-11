79ffe3fbd7bbfb9b974dfb73462420d5baa565fa
ernie117 <ern3st@hotmail.co.uk>
2021-01-11 17:34:05 +0000

    commenting out ssl stuff

--- functional-tests/features/environment.py
+++ functional-tests/features/environment.py
@@ -1,23 +1,23 @@
 from behave import fixture, use_fixture
-from OpenSSL import crypto
+# from OpenSSL import crypto
 import os
 
 
 # Not required for now but if I ever need to run tests against prod in HTTPS
 # with a self-signed cert
-@fixture(name="requests.ssl.certificate")
-def ssl_cert_setup(context):
-    context.pem = "cert.pem"
-    with open("../src/main/resources/keystore.p12", "rb") as f:
-        keystore = f.read()
-
-    p12 = crypto.load_pkcs12(keystore, os.getenv("USER_PASSWORD_PLAIN"))
-    cert = crypto.dump_certificate(crypto.FILETYPE_PEM, p12.get_certificate())
-    with open(context.pem, "wb") as f:
-        f.write(cert)
-
-    yield context.pem
-    os.remove(context.pem)
+# @fixture(name="requests.ssl.certificate")
+# def ssl_cert_setup(context):
+#     context.pem = "cert.pem"
+#     with open("../src/main/resources/keystore.p12", "rb") as f:
+#         keystore = f.read()
+#
+#     p12 = crypto.load_pkcs12(keystore, os.getenv("USER_PASSWORD_PLAIN"))
+#     cert = crypto.dump_certificate(crypto.FILETYPE_PEM, p12.get_certificate())
+#     with open(context.pem, "wb") as f:
+#         f.write(cert)
+#
+#     yield context.pem
+#     os.remove(context.pem)
 
 
 def auth_setup(context):
